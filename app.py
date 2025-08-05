07cb77eda87d079bf779638a422f21b024a54d04
KS-prog-ds <kanchan.son@gmail.com>
2025-08-05 18:21:37 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import sys
+from config import DB_DETAILS
+
+def main():
+    #for arg in sys.argv:
+     env = sys.argv[1]
+     db_details = DB_DETAILS[env]
+     print(db_details)
+if __name__ == '__main__':
+    main()
+
+####run -> config add db user & db pass
